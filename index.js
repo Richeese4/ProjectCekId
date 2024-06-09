347abc6298c19ba74223ee8b14371ee6e3e11442
Richeese4 <78680000+Richeese4@users.noreply.github.com>
2024-06-09 13:38:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Put your code here!!
+addEventListener('fetch', event => { event.respondWith(sendRequest(event.request))
+})
+async function sendRequest(request) {
+  const url = new URL(request.url)
+  const path = url.pathname
+  const params = url.searchParams
+  const endpoint = 'https://order-sg.codashop.com/initPayment.action'
+  const id = params.get('id')
+  const zone = params.get('zone')
+  try {
+    if (!id) {
+      return new Response('Bad request')
+    }
+    // Kode utama akan ada disini
+    else {
+      return new Response('Bad request')
+    }
+  }
+  catch (error) {
+    return new Response('Not found')
+  }
+}
