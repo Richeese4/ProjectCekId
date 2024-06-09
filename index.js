631fd735187acb94405ac41df2749dd44d40a005
Richeese4 <78680000+Richeese4@users.noreply.github.com>
2024-06-09 16:35:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ async function sendRequest(request) {
       return new Response('Bad request')
     }
     // Kode utama akan ada disini
+      if (path.includes('/ff')) {
+      const body = `voucherPricePoint.id=8050&voucherPricePoint.price=1000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=FREEFIRE&shopLang=id_ID&voucherTypeId=1&gvtId=1`
+      const request = new Request(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body
+      })
+      const response = await fetch(request)
+      const data = await response.json()
+      return new Response(data.confirmationFields.roles[0].role)
+    }
     else {
       return new Response('Bad request')
     }
