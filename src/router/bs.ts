952fe1cc4be0e17f5da9d2a8aa0678ba25883de1
Richeese4 <78680000+Richeese4@users.noreply.github.com>
2025-04-04 03:03:29 +0700

    Update bs.ts

--- src/router/bs.ts
+++ src/router/bs.ts
@@ -7,6 +7,6 @@ export default async function ff(id: number): Promise<Result> {
     success: true,
     game: 'Blood Strike',
     id,
-    name: data.confirmationFields.roles[0].role
+    name: data.confirmationFields.username
   }
 }
