314200a6f7971641fc65e626bc72b96b03d9fa25
Richeese4 <78680000+Richeese4@users.noreply.github.com>
2025-04-03 16:01:21 +0700

    Update bp.ts

--- src/router/bp.ts
+++ src/router/bp.ts
@@ -7,6 +7,6 @@ export default async function bp(id: number): Promise<Result> {
     success: true,
     game: '8 Ball Pool',
     id,
-    name: data.confirmationFields.roles[0].role
+    name: data.confirmationFields.username
   }
 }
