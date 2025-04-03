655b28bb635594a08adaa47550fdc283a87e13e7
Richeese4 <78680000+Richeese4@users.noreply.github.com>
2025-04-04 03:35:39 +0700

    Update bs.ts

--- src/router/bs.ts
+++ src/router/bs.ts
@@ -1,7 +1,7 @@
 import { hitCoda, Result } from '../utils'
 
 export default async function bs(id: number): Promise<Result> {
-  const body = `voucherPricePoint.id=895131&voucherPricePoint.price=14000&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=BLOOD_STRIKE&shopLang=id_IDvoucherTypeId=358&gvtId=508`
+  const body = `voucherPricePoint.id=895131&voucherPricePoint.price=14000&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=BLOOD_STRIKE&shopLang=id_ID&voucherTypeId=358&gvtId=508`
   const data = await hitCoda(body)
   return {
     success: true,
