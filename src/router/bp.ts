bb7fc2310f50732b60590de798349ab15856a9de
Richeese4 <78680000+Richeese4@users.noreply.github.com>
2025-04-03 15:22:50 +0700

    Create bp.ts

---
+++ src/router/bp.ts
@@ -0,0 +1,12 @@
+import { hitCoda, Result } from '../utils'
+
+export default async function bp(id: number): Promise<Result> {
+  const body = `voucherPricePoint.id=205527&voucherPricePoint.price=14000&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=EIGHT_BALL_POOL&shopLang=id_ID`
+  const data = await hitCoda(body)
+  return {
+    success: true,
+    game: '8 Ball Pool',
+    id,
+    name: data.confirmationFields.roles[0].role
+  }
+}
