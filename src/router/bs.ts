9dc28e2bc87a2b3b2bcac23fee9b50de2cac5267
Richeese4 <78680000+Richeese4@users.noreply.github.com>
2025-04-03 14:57:28 +0700

    Create bs.ts

---
+++ src/router/bs.ts
@@ -0,0 +1,12 @@
+import { hitCoda, Result } from '../utils'
+
+export default async function bs(id: number): Promise<Result> {
+  const body = `voucherPricePoint.id=895131&voucherPricePoint.price=14000&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=BLOOD_STRIKE&shopLang=id_ID`
+  const data = await hitCoda(body)
+  return {
+    success: true,
+    game: 'Blood Strike',
+    id,
+    name: data.confirmationFields.roles[0].role
+  }
+}
